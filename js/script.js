faa14fcdf935efe6c0c158ae4ed4ef9b9c03f1bb
rahuljaiswar <53975431+rahuljaiswar@users.noreply.github.com>
2023-01-11 12:04:51 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    var scrollValue = window.scrollY;
+    if (scrollValue < 450) {
+        navbar.classList.remove('bgcolor');
+    } else {
+        navbar.classList.add('bgcolor');
+    }
+}
+window.addEventListener('scroll', myFunction);
+
+
+var preloader = document.getElementById('loader');
+function loading(){
+    preloader.style.display = 'none';
+}
